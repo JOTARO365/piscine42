0a81106df00e1ae4bf3f8304776db2fc57b1464b
Wisanu Aon-In <108912837+JOTARO365@users.noreply.github.com>
2025-05-21 20:53:58 +0700

    Add files via upload

--- c/c00/ex07/ft_putnbr.c
+++ c/c00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: wiaon-in <wiaon-in@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 20:09:19 by wiaon-in          #+#    #+#             */
-/*   Updated: 2025/05/21 00:33:19 by wiaon-in         ###   ########.fr       */
+/*   Updated: 2025/05/21 18:03:24 by wiaon-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putnbr(int n)
+void	ft_putnbr(int nb)
 {
-	if (n == -2147483648)
+	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
 		return ;
 	}
-	if (n < 0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		n = -n;
+		nb = -nb;
 	}
-	if (n > 9)
+	if (nb > 9)
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
 	else
 	{
-		ft_putchar(n + '0');
+		ft_putchar(nb + '0');
 	}
 }
